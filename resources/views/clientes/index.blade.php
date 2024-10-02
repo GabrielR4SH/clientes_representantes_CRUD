2abfb7b4b996a27a4dc448e31b5f699cb3394e90
GabrielR4SH <bieelsantana23@hotmail.com>
2024-10-01 23:44:54 -0300

    feat: table modify

--- resources/views/clientes/index.blade.php
+++ resources/views/clientes/index.blade.php
@@ -3,52 +3,45 @@
 @section('title', 'Lista de Clientes')
 
 @section('content')
-<h1 class="text-xl font-bold mb-4">Lista de Clientes</h1>
-<div class="overflow-x-auto">
-    <table class="min-w-full bg-white dark:bg-gray-800">
-        <thead class="bg-gray-200 dark:bg-gray-700">
-            <tr>
-                <th class="py-3 px-4 text-left text-xs font-medium text-gray-700 dark:text-white uppercase tracking-wider">
-                    Nome
-                </th>
-                <th class="py-3 px-4 text-left text-xs font-medium text-gray-700 dark:text-white uppercase tracking-wider">
-                    CPF
-                </th>
-                <th class="py-3 px-4 text-left text-xs font-medium text-gray-700 dark:text-white uppercase tracking-wider">
-                    Idade
-                </th>
-                <th class="py-3 px-4 text-left text-xs font-medium text-gray-700 dark:text-white uppercase tracking-wider">
-                    Sexo
-                </th>
-                <th class="py-3 px-4 text-left text-xs font-medium text-gray-700 dark:text-white uppercase tracking-wider">
-                    Cidade
-                </th>
-                <th class="py-3 px-4 text-left text-xs font-medium text-gray-700 dark:text-white uppercase tracking-wider">
-                    Ações
-                </th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($clientes as $cliente)
-            <tr class="border-b dark:border-gray-700">
-                <td class="py-4 px-4">{{ $cliente->nome }}</td>
-                <td class="py-4 px-4">{{ $cliente->cpf }}</td>
-                <td class="py-4 px-4">{{ $cliente->idade }}</td>
-                <td class="py-4 px-4">{{ $cliente->sexo }}</td>
-                <td class="py-4 px-4">{{ $cliente->cidade ? $cliente->cidade->nome : 'N/A' }}</td>
-                <td class="py-4 px-4">
-                    <a href="{{ route('clientes.edit', $cliente->id) }}" class="bg-blue-500 text-white px-2 py-1 rounded">Editar</a>
-                    <form action="{{ route('clientes.destroy', $cliente->id) }}" method="POST" class="inline-block" onsubmit="return confirm('Tem certeza que deseja deletar este cliente?');">
-                        @csrf
-                        @method('DELETE')
-                        <button type="submit" class="btn btn-danger">Deletar</button>
-                    </form>
-                </td>
-            </tr>
-            @endforeach
-        </tbody>
-    </table>
-    <br>
-    {{ $clientes->links() }}
+<h1 class="text-center font-weight-bold mb-4">Lista de Clientes</h1>
+<div class="table-responsive">
+    <div class="container d-flex justify-content-center">
+        <table class="table table-bordered table-striped table-hover bg-white">
+            <thead class="thead-light">
+                <tr>
+                    <th scope="col">Nome</th>
+                    <th scope="col">CPF</th>
+                    <th scope="col">Idade</th>
+                    <th scope="col">Sexo</th>
+                    <th scope="col">Cidade</th>
+                    <th scope="col">Ações</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($clientes as $cliente)
+                <tr>
+                    <td>{{ $cliente->nome }}</td>
+                    <td>{{ $cliente->cpf }}</td>
+                    <td>{{ $cliente->idade }}</td>
+                    <td>{{ $cliente->sexo }}</td>
+                    <td>{{ $cliente->cidade ? $cliente->cidade->nome : 'N/A' }}</td>
+                    <td>
+                        <div class="btn-group" role="group">
+                            <a href="{{ route('clientes.edit', $cliente->id) }}" class="btn btn-primary">Editar</a> 
+                            <form action="{{ route('clientes.destroy', $cliente->id) }}" method="POST" onsubmit="return confirm('Tem certeza que deseja deletar este cliente?');">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit" class="btn btn-danger">Deletar</button>
+                            </form>
+                        </div>
+                    </td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+    <div class="py-4 d-flex justify-content-center">
+        {{ $clientes->links('pagination::bootstrap-4') }}
+    </div>
 </div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,5 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ClienteController;
 
 Route::get('index', [ClienteController::class, 'index'])->name('clientes.index');
-
 Route::get('clientes/{id}/edit', [ClienteController::class, 'edit'])->name('clientes.edit');
 Route::delete('clientes/{id}', [ClienteController::class, 'destroy'])->name('clientes.destroy');
